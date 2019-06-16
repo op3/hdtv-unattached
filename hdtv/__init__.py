38c3643fd460c82dff9ffeb9f05cf67f4f3087c7
Oliver Papst <opapst@ikp.tu-darmstadt.de>
2019-06-16 17:15:11 +0200

    Fix hdtv.rootext imports
    If ROOT.HDTV.* is used, it is necessary to import the corresponding
    rootext library.

--- hdtv/__init__.py
+++ hdtv/__init__.py
@@ -1,5 +1,11 @@
 import os
 
+# This needs to happen before "import ROOT" is called the first time –
+# if not, there will be a header not found error
+# for *some* ROOT versions and install methods
+import hdtv.rootext
+hdtv.rootext.UpdateRootIncludePath()
+
 installdir = os.path.dirname(__file__)
 datadir = os.path.join(installdir, "share")
 clibpath = os.path.join(installdir, "clib")
